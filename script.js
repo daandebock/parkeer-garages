5387df90bee6a2d9404d47d0eb92cbdd098fc4e4
daandebock <48532697+daandebock@users.noreply.github.com>
2024-03-08 16:35:40 +0100

    Parking

---
+++ script.js
@@ -0,0 +1,24 @@
+// url api /api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?limit=20
+
+const url = "https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?limit=20";
+
+const plek = document.querySelector("#accordionExample");
+
+async function api() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    data.results.forEach((element, index) => {
+        const test = document.createElement("div");
+        test.innerHTML = '<div class="accordion-item"><h2 class="accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"data-bs-target="#collapse' + index + '" aria-expanded="true" aria-controls="collapseOne">' + element.name + '</button></h2><div id="collapse' + index + '" class="accordion - collapse collapse" data-bs-parent="#accordionExample"><div class="accordion - body"><strong>Beschikbare parkeerplaatsen</strong><br>' + element.availablecapacity + '</div></div></div>';
+
+        plek.appendChild(test);
+
+    });
+
+
+
+
+};
+
+api();
