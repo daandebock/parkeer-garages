807eb4dad892e18c8ab4546bf96f5e5c352231b5
daandebock <48532697+daandebock@users.noreply.github.com>
2024-03-09 12:41:44 +0100

    added sorting for availablecapacity in dom

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ async function api() {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
+    data.results.sort(function (a, b) {
+        return b.availablecapacity - a.availablecapacity;
+    });;
     data.results.forEach((element, index) => {
         const test = document.createElement("div");
-        test.innerHTML = '<div class="accordion-item"><h2 class="accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"data-bs-target="#collapse' + index + '" aria-expanded="true" aria-controls="collapseOne">' + element.name + '</button></h2><div id="collapse' + index + '" class="accordion - collapse collapse" data-bs-parent="#accordionExample"><div class="accordion - body"><strong>Beschikbare parkeerplaatsen</strong><br>' + element.availablecapacity + '</div></div></div>';
+        test.innerHTML = '<div class="accordion-item"><h2 class="accordion-header"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"data-bs-target="#collapse' + index + '" aria-expanded="true" aria-controls="collapseOne">' + element.name + '</button></h2><div id="collapse' + index + '" class="accordion - collapse collapse" data-bs-parent="#accordionExample"><div class="accordion - body"><strong>Beschikbare parkeerplaatsen</strong><br>' + element.availablecapacity + ' van de ' + element.totalcapacity + '</div ></div ></div > ';
 
         plek.appendChild(test);
 
